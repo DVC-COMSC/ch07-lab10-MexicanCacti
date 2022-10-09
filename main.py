e06aa52f4d74e4c173df738dd0df443e07adc369
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 19:01:09 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ for i in range(len(values)):
     for k in range(i,len(values),1):
         if int(values[min]) > int(values[k]):
             min = k
-    if min == 0:
+    if min == i:
         print(values)
         continue
     else:
@@ -19,12 +19,3 @@ for i in range(len(values)):
         values[i] = values[min]
         values[min] = temp
         print(values)
-
-
-
-#for j in range(i + 1,len(values),1):
-        ##if int(values[j]) < int(values[i]):
-            ##temp = values[i]
-            ##values[i] = values[j]
-            ##values[j] = temp
-            ##print(values)
