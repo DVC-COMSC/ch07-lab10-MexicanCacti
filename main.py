9d8371973a414a49278a781912f52ac3ffb05c69
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 18:40:24 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # ******************************
 # Make your Code
 # ******************************
+value = input().split()
+values = []
+
+for i in range(len(value)):
+    values.append(value[i])
+
+for i in range(len(values)):
+    for j in range(i + 1,len(values),1):
+        if int(values[j]) < int(values[i]):
+            temp = values[i]
+            values[i] = values[j]
+            values[j] = temp
+            print(values)
